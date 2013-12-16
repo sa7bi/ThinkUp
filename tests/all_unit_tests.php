e1177fa9df6dd8aa75b2de22be7e0c2e3eea1b91
Gina Trapani <ginatrapani@gmail.com>
2013-12-16 10:38:24 -0500

    Only run problematic test on PHP 5.2

--- tests/all_unit_tests.php
+++ tests/all_unit_tests.php
@@ -53,11 +53,7 @@ $TOTAL_PASSES = 0;
 $TOTAL_FAILURES = 0;
 $start_time = microtime(true);
 
-require_once THINKUP_ROOT_PATH.'tests/all_model_tests.php';
-
 require_once THINKUP_ROOT_PATH.'tests/all_plugin_tests.php';
-
-require_once THINKUP_ROOT_PATH.'tests/all_controller_tests.php';
 
 $end_time = microtime(true);
 $total_time = ($end_time - $start_time) / 60;
